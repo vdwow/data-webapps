9cba0695e1565cb8279ac60681c913f6b01a234f
Valentin D <valentin.defour@gmail.com>
2022-01-31 12:13:12 +0100

    Create youtube_downloader.py

---
+++ youtube-downloader/youtube_downloader.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from datetime import date
+from pytube import YouTube
+
+st.set_page_config(layout="wide")
+
+st.title('Youtube Downloader')
+
+st.subheader("Enter the URL:")
+url = st.text_input(label='URL')
+
+yt = YouTube(url)
+
+print(yt.streams)
+
+if url != '':
+    yt = YouTube(url)
+    st.image(yt.thumbnail_url, width=300)
+    
+    st.subheader("Title : " + yt.title)
+    
+    st.text('''
+    Length: {} seconds
+    Rating: {} 
+    '''.format(yt.length , yt.rating))
+
+    video = yt.streams
+
+    if len(video) > 0:
+        downloaded , download_audio = False , False
+        download_video = st.button("Download Video")
+        if yt.streams.filter(only_audio=True):
+            download_audio = st.button("Download Audio Only")
+        if download_video:
+            video.get_lowest_resolution().download()
+            downloaded = True
+        if download_audio:
+            video.filter(only_audio=True).first().download()
+            downloaded = True
+        if downloaded:
+            st.subheader("Download Complete")
+    else:
+        st.subheader("Sorry, this video can not be downloaded")
