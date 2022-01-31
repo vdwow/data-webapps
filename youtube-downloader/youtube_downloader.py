f4d7481a895272903d5abab995e1a5973b3e1834
Valentin D <valentin.defour@gmail.com>
2022-01-31 15:30:43 +0100

    Update youtube_downloader.py

--- youtube-downloader/youtube_downloader.py
+++ youtube-downloader/youtube_downloader.py
@@ -3,8 +3,9 @@ import pandas as pd
 import numpy as np
 from datetime import date
 from pytube import YouTube
+from zipfile import ZipFile
 
-st.set_page_config(page_title = "Youtbe Downloader", page_icon = "random")   #layout="wide")
+st.set_page_config(page_title = "Youtube Downloader", page_icon = "🤖")   #layout="wide")
 
 st.title('Youtube Downloader')
 
@@ -17,7 +18,7 @@ print(yt.streams)
 
 if url_video != '':
     yt = YouTube(url_video)
-    
+
     st.image(yt.thumbnail_url, width=300)
     
     st.subheader("Title : " + yt.title)
@@ -29,18 +30,44 @@ if url_video != '':
 
     video = yt.streams
 
-    if len(video) > 0:
-        downloaded , download_audio = False , False
-        download_video = st.button("Download Video")
-        if yt.streams.filter(only_audio=True):
-            download_audio = st.button("Download Audio Only")
-        if download_video:
-            video.get_lowest_resolution().download(r"C:/Users/Public/Videos")
-            downloaded = True
-        if download_audio:
-            video.filter(only_audio=True).first().download()
-            downloaded = True
-        if downloaded:
-            st.subheader("Download Complete")
-    else:
-        st.subheader("Sorry, this video can not be downloaded")
+    # btn = st.download_button(
+    #         label="Download VIDEO",
+    #         data=video.get_lowest_resolution(),
+    #         file_name="video"
+    #         #mime="application/zip"
+    #     )
+
+    # if len(video) > 0:
+
+    #     downloaded , download_audio = False , False
+    #     download_video = st.button("Download Video")
+
+    #     if yt.streams.filter(only_audio=True):
+    #         download_audio = st.button("Download Audio Only")
+
+    #     if download_video:
+    #         video.get_lowest_resolution().download()
+    #         downloaded = True
+
+    #     if download_audio:
+    #         video.filter(only_audio=True).first().download()
+    #         downloaded = True
+
+    #     if downloaded:
+    #         st.subheader("Download Complete")
+
+    # else:
+    #     st.subheader("Sorry, this video can not be downloaded")
+
+    zip_file = ZipFile('video.zip', 'w')
+    zip_file.write(video.get_lowest_resolution().download())
+    #zip_file.write('yt.srt')
+    zip_file.close()
+
+    with open("video.zip", "rb") as zip_download:
+        btn = st.download_button(
+            label="Download VIDEO",
+            data=zip_download,
+            file_name="video.zip",
+            mime="application/zip"
+        )
